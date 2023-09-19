aecd0c65e6160875915754394ce9eeac4b4dc434
SquarerAtol <76865028+SquarerAtol@users.noreply.github.com>
2023-09-19 14:28:45 +0900

    1 file added

---
+++ src/AppMemo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AppMemo();
+const title='team';
+
+return (
+  <div>
+    <h1>{title}</h1>
+    <img src="movie1.jpg" width="100"/>
+  </div>
+);
+
